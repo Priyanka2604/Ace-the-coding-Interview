1876a1bc1b9ca5587c30933fe2cf44ac4f9ee49c
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-16 21:15:19 +0530

    Time: 0 ms (100%), Space: 61.1 MB (5.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,22 +3,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let n = nums.length, c = 0, i=0, j=0;
-    let arr = new Array(n);
-    for(i=0,j=0;i<n;i++){
+    let n = nums.length, j = -1;
+    for(let i=0;i<n;i++){
         if(nums[i]===0){
-            c++;
-        } else{
-            arr[j] = nums[i];
+            j = i;
+            break;
+        }
+    }
+
+    if(j === -1) return nums;
+
+    for(let i=j+1;i<n;i++){
+        if(nums[i]!==0){
+            nums[j] = nums[i];
+            nums[i] = 0;
             j++;
         }
     }
 
-    while(c-->0){
-        arr[j++] = 0
-    }
-
-    for(let i=0;i<n;i++){
-        nums[i] = arr[i];
-    }
+    return nums;
 };
