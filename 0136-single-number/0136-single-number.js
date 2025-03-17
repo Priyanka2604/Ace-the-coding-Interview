bd1056353bf6dbf9ff71ae3337968bc5246c94a0
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-17 14:34:50 +0530

    Time: 0 ms (100%), Space: 54.6 MB (46.74%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,16 +3,10 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let mp = new Map();
-    for(let i=0; i<nums.length; i++) {
-        mp.set(nums[i], (mp.get(nums[i]) || 0)+1);
+    let r = 0;
+    for(let i=0;i<nums.length;i++){
+        r ^= nums[i];
     }
 
-    for(const [key, val] of mp) {
-        if(val === 1){
-            return parseInt(key);
-        }
-    }
-
-    return -1;
+    return r;
 };
