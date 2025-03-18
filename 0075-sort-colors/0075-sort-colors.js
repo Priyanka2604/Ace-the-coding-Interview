a4ba72a24e00dfccc914ac0ffceba07173477806
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-18 23:34:45 +0530

    Time: 0 ms (100%), Space: 53.9 MB (22.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,7 +3,22 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let res = nums.sort();
-    nums = res;
-    return res;
+    let r=0, w=0, b = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===0){ r++;}
+        else if(nums[i]===1){ w++; }
+        else{ b++; }
+    }
+
+    for(let i=0;i<r;i++){
+        nums[i] = 0;
+    }
+
+    for(let i=r;i<(r+w);i++){
+        nums[i] = 1;
+    }
+
+    for(let i=(r+w);i<nums.length;i++){
+        nums[i] = 2;
+    }
 };
