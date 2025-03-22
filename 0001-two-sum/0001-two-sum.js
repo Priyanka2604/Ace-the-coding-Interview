4be80b07c6bcd0ae698f3085fd85a2fb56061292
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-22 21:09:52 +0530

    Time: 1 ms (89.01%), Space: 55 MB (25.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,9 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let mp = new Map();
     for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(i!==j && nums[i]+nums[j]===target) return [i,j];
+        if(mp.has(target-nums[i])){
+            return [i, mp.get(target-nums[i])];
+        } else {
+            mp.set(nums[i],i);
         }
     }
+
+    return [-1, -1];
 };
