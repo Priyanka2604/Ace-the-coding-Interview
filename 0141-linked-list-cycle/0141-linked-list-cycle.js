2c14097ddd2af57e073685dd5621c63f25ba7630
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-29 01:19:44 +0530

    Time: 55 ms (60.79%), Space: 60.2 MB (5.66%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let hs = new Set();
+    while(head!==null){
+        if(hs.has(head)){
+            return true;
+        } 
+        hs.add(head);
+        head = head.next;
+    }
+
+    return false;
+};
