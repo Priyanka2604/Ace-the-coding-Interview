51f57c592e1410bb871d76f75000945165a32d74
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-03-29 11:14:45 +0530

    Time: 0 ms (100%), Space: 56.1 MB (58.38%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -4,17 +4,20 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
- */ // 3 -> 2 -> 1 -> null
+ */
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var reverseList = function(head) {
     if(head===null || head.next===null) return head;
-    let newHead = reverseList(head.next);
-    var curr = head.next;
-    curr.next = head;
-    head.next = null;
+    let temp = head, prev = null;
+    while(temp!==null){
+        let curr = temp.next;
+        temp.next = prev;
+        prev = temp;
+        temp = curr;
+    }
 
-    return newHead;
+    return prev;
 };
