7a845d0d83f2f5753b73e08afbb725581c71f68f
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-04-03 16:41:29 +0530

    Time: 2 ms (74.7%), Space: 59.5 MB (23.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -4,12 +4,14 @@
  */
 var setZeroes = function(matrix) {
     let m = matrix.length;
+    let rowArr = new Array(m).fill(0);
+    let colArr = new Array(matrix[0].length).fill(0);
     for(let i=0;i<m;i++){
         let n = matrix[i].length;
         for(let j=0;j<n;j++){
             if(matrix[i][j]===0){
-                markRow(matrix,n,i);
-                markCol(matrix,m,j);
+                rowArr[i] = 1;
+                colArr[j] = 1;
             }
         }
     }
@@ -17,25 +19,12 @@ var setZeroes = function(matrix) {
     for(let i=0;i<m;i++){
         let n = matrix[i].length;
         for(let j=0;j<n;j++){
-            if(matrix[i][j]=== _){
+            if(rowArr[i]===1){
+                matrix[i][j] = 0;
+            }
+            if(colArr[j]===1){
                 matrix[i][j] = 0;
             }
         }
     }
 };
-
-var markRow = function(matrix,n,i) {
-    for(let j=0;j<n;j++){
-        if(matrix[i][j]!==0){
-            matrix[i][j] = _;
-        }
-    }
-}
-
-var markCol = function(matrix,n,j) {
-    for(let i=0;i<n;i++){
-        if(matrix[i][j]!==0){
-            matrix[i][j] = _;
-        }
-    }
-}
