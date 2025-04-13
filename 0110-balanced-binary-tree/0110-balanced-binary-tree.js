e00a8993e3ddde0ba98bc89538985893c99e2c24
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-04-14 00:01:13 +0530

    Time: 1 ms (89.31%), Space: 60.3 MB (53.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,28 +11,15 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    if(!root) return true;
-    let lh = getHeight(root.left);
-    let rh = getHeight(root.right);
-    if(Math.abs(lh-rh)<=1 && isBalanced(root.left) && isBalanced(root.right)){
-        return true;
-    }
-
-    return false;
+    return dfs(root) !== -1;
 };
 
-var getHeight = (root) => {
+var dfs = (root) => {
     if(!root) return 0;
-    let q = [], level = 0;
-    q.push(root);
-    while(q.length>0){
-        const qlen = q.length;
-       for(let i=0;i<qlen;i++){
-            const curr = q.shift();
-            if(curr.left) q.push(curr.left);
-            if(curr.right) q.push(curr.right);
-        }
-        level++;
-    }
-    return level;
+    let lh = dfs(root.left);
+    if(lh===-1) return -1;
+    let rh = dfs(root.right);
+    if(rh===-1) return -1;
+    if(Math.abs(lh-rh)>1) return -1;
+    return Math.max(lh,rh)+1;
 }
