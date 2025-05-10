5ac9f0cc0fd1a6ec51d15618a2b6e78c295e45a7
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-05-10 20:44:16 +0530

    Time: 582 ms (53.92%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT employee_id, department_id
-FROM Employee
-WHERE primary_flag = 'Y'
-UNION
-SELECT employee_id, department_id FROM Employee
-GROUP BY employee_id
-HAVING COUNT(employee_id) = 1;
+FROM Employee e
+WHERE primary_flag = 'Y' OR
+NOT EXISTS(
+    SELECT * FROM Employee
+    WHERE e.employee_id = employee_id
+    and e.department_id <> department_id);
