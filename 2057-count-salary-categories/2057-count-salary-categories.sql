4f042b3b2038c50338e82dc4fbbd2cdc8fb9bf53
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-05-11 13:53:08 +0530

    Time: 1693 ms (40.64%), Space: 0B (100%) - LeetHub

--- 2057-count-salary-categories/2057-count-salary-categories.sql
+++ 2057-count-salary-categories/2057-count-salary-categories.sql
@@ -1,15 +1,23 @@
 # Write your MySQL query statement below
+WITH Categories AS (
+    SELECT 'Low Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary'
+    UNION ALL
+    SELECT 'High Salary'
+)
 SELECT 
-    'Low Salary' AS category,
-    COUNT(CASE WHEN income<20000 THEN 1 END) AS accounts_count
-FROM Accounts
-UNION ALL
-SELECT 
-    'Average Salary' AS category,
-    COUNT(CASE WHEN income>=20000 AND income<=50000 THEN 1 END) AS accounts_count
-FROM Accounts
-UNION ALL
-SELECT 
-    'High Salary' AS category,
-    COUNT(CASE WHEN income>50000 THEN 1 END) AS accounts_count
-FROM Accounts;
+    c.category, 
+    IFNULL(a.accounts_count, 0) AS accounts_count
+FROM Categories c
+LEFT JOIN (
+    SELECT 
+        CASE 
+            WHEN income < 20000 THEN 'Low Salary'
+            WHEN income BETWEEN 20000 AND 50000 THEN 'Average Salary'
+            ELSE 'High Salary'
+        END AS category, 
+        COUNT(*) AS accounts_count
+    FROM Accounts
+    GROUP BY category
+) a ON c.category = a.category;
