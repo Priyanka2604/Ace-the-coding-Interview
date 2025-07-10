41ce58091c844e17da25d27a8e2365be6e14431f
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-11 03:12:19 +0530

    Time: 6 ms (93.05%), Space: 55.8 MB (90.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,7 +5,15 @@
  */
 var isAnagram = function(s, t) {
     if(s.length!==t.length) return false;
-    if(s.split('').sort().join('') !== t.split('').sort().join('')) return false;
+    // if(s.split('').sort().join('') !== t.split('').sort().join('')) return false;
+    const a = "a".charCodeAt(0);
+    let alphabets = new Array(26).fill(0);
 
-    return true;
+    for(let i=0;i<s.length;i++){
+        alphabets[s.charCodeAt(i) - a]++;
+        alphabets[t.charCodeAt(i) - a]--;
+
+    }
+
+    return alphabets.every(c => c === 0);
 };
