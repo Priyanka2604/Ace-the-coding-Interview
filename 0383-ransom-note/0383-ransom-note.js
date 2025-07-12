2edc86dfc11d66c1a5c7484e14b31605b06a5179
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-13 02:35:18 +0530

    Time: 14 ms (78.55%), Space: 60 MB (28.88%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,18 +4,10 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const mag = [...magazine];
-    const note = [...ransomNote];
-
-    while(mag.length>0){
-        let c = mag.pop();
-        let indx = note.indexOf(c);
-        if(indx!==-1){
-            note.splice(indx, 1);
-        }
-    }
-
-    const ans = note.length ? false : true;
-
-    return ans;
+    for (const char of magazine) {
+    ransomNote = ransomNote.replace(char, "");
+  }
+  
+  if (!ransomNote) return true;
+  else return false;
 };
