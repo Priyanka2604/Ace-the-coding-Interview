e015a8f2b5a7ed1e456dd4b521df2337116a48da
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-15 13:09:53 +0530

    Time: 44 ms (5.53%), Space: 57.7 MB (5.23%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function(s) {
+    let mp = new Map();
+
+    for(const i in s){
+        mp.set(s[i], (mp.get(s[i]) || 0)+1);
+    }
+
+    let c = 0;
+    for(const x of mp.values()){
+        if(x%2===0){
+            c += x;
+        } else {
+            c += x-1;
+        }
+    }
+
+    return c < s.length ? c+1 : c;
+};
