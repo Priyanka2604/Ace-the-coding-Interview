8623cbad77a4b34aaef004eb26aa94fc6d5aee1f
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-19 00:23:00 +0530

    Time: 0 ms (100%), Space: 53.1 MB (68.61%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.js
@@ -2,11 +2,12 @@
  * @param {string} s
  * @return {number}
  */
+ //In this efficient approach, we iterate through the characters of the string, and whenever we encounter a non-space character that is preceded by a space or is at the beginning of the string, we increment the count. This avoids splitting the string and is more efficient for large input strings with many spaces
+ 
 var countSegments = function(s) {
-    let sarr = s.split(" ");
     let c = 0;
-    for(let i of sarr){
-        if(i.length>0){
+    for(let i=0; i<s.length; i++){
+        if(s[i] !== ' ' && (i===0 || s[i-1] === ' ')){
             c++;
         }
     }
