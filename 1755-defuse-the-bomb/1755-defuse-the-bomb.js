ef5ab06a4a7a32d42e64ec09ff01917edc7f9ee6
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-24 01:49:30 +0530

    Time: 0 ms (100%), Space: 53.4 MB (93.19%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.js
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.js
@@ -4,27 +4,27 @@
  * @return {number[]}
  */
 var decrypt = function(code, k) {
-    let n = code.length;
-    let ans = new Array(n).fill(0);
-    for(let i = 0; i < n; i++){
-        let j = 0, c = 0;
-        if(k > 0){
-            j = i + 1;
-            while(c++ < k){
-                if(j === n) j = 0;
-                ans[i] += code[j];
-                j++;
-            }
-        } else if (k < 0){
-            j = i - 1;
-            while(c-- > k){
-                if(j === -1) j = n - 1;
-                ans[i] += code[j];
-                j--;
-            }
+    const decode = (code, k) => {
+        const res = new Array(code.length).fill(0);
+        let sum = 0;
+
+        for(let i = 0; i< k; i++){
+            sum += code[i];
         }
 
+        for(let i = 0; i < code.length; i++){
+            sum = sum - code[i] + code[(i+k) % code.length];
+            res[i] = sum;
+        }
+
+        return res;
     }
 
-    return ans;
+    return k > 0 ? decode(code, k) : decode(code.reverse(), -k).reverse();
 };
+
+/**
+5 7 1 4.     2, -2.   4 1 7 5
+8 5 9 12.             8 12 9 5
+5 9 12 8              5 9 12 8
+ */
