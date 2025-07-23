4c565fa0622caa62bae6236054a939e4fa48a66c
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-23 14:25:10 +0530

    Time: 35 ms (52.11%), Space: 60.9 MB (79.16%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
@@ -3,15 +3,13 @@
  * @return {number}
  */
 var findLHS = function(nums) {
-    const mp = new Map();
-    for(const num of nums){
-        mp.set(num, (mp.get(num) || 0) + 1);
-    }
+    nums.sort( (a, b) => a - b);
+    let j = 0, maxLen = 0;
 
-    let maxLen = 0;
-    for(let [num, freq] of mp.entries()){
-        if(mp.has(num+1)){
-            maxLen = Math.max(maxLen, freq + mp.get(num+1));
+    for(let i = 0; i < nums.length; i++) {
+        while(nums[i] - nums[j] > 1) j++;
+        if(nums[i] - nums[j] === 1) {
+            maxLen = Math.max(maxLen, i - j + 1);
         }
     }
 
