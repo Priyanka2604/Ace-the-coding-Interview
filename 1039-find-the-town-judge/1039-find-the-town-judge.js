8315ffcfa6c7f32f380284115ffd7a78162b1cbb
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-25 20:56:55 +0530

    Time: 4 ms (77.27%), Space: 63.6 MB (43.43%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(n, trust) {
+    let inDegree = new Array(n+1).fill(0);
+    let outDegree = new Array(n+1).fill(0);
+
+    for(let i of trust){
+        inDegree[i[1]]++;
+        outDegree[i[0]]++;
+    }
+
+    for(let i = 1; i <= n; ++i){
+        if(inDegree[i] === n-1 && outDegree[i] === 0){
+            return i;
+        }
+    }
+
+    return -1;
+};
