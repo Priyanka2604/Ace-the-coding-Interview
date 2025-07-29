7ef25bd41572705633554712a1374f337560833b
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-29 14:26:57 +0530

    Time: 6 ms (69.4%), Space: 56.7 MB (86.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let set = new Set();
+    let map = new Map();
     let left = 0;
     let maxLen = 0;
 
     if(s.length < 2) return s.length;
 
-    for(let i=0; i<s.length; i++){
-        while(set.has(s[i])){
-            set.delete(s[left])
-            left++
+    for(let i = 0; i < s.length; i++) {
+        if(map.has(s[i]) && map.get(s[i]) >= left) {
+            left = map.get(s[i]) + 1;
         }
-        set.add(s[i]);
-        maxLen = Math.max(maxLen, i-left+1)
+        map.set(s[i], i)
+        maxLen = Math.max(maxLen, i - left + 1);
     }
 
     return maxLen;
