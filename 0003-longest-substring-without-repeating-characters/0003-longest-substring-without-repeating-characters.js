cf3739a8f42b7be333932dae61a4322f3fbc13aa
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-29 14:21:29 +0530

    Time: 9 ms (41.09%), Space: 58.8 MB (50.17%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let left = 0;
+    let maxLen = 0;
+
+    if(s.length < 2) return s.length;
+
+    for(let i=0; i<s.length; i++){
+        while(set.has(s[i])){
+            set.delete(s[left])
+            left++
+        }
+        set.add(s[i]);
+        maxLen = Math.max(maxLen, i-left+1)
+    }
+
+    return maxLen;
+};
