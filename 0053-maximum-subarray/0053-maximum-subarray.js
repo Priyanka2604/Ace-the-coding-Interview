5f7cbb365c9ea8737060f313efcc28a92ea3e42a
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-07-29 14:55:07 +0530

    Time: 3 ms (44.18%), Space: 64.2 MB (23.61%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    // Initialize currMaxSum & take first element of array from which we start to do sum...
+    maxSum = nums[0];
+    // Initialize the current sum of our subarray as nums[0]...
+    currSum = nums[0];
+    // Traverse all the element through the loop...
+    for (let i = 1; i < nums.length; i++) {
+        // Do sum of elements contigous with curr sum...
+        // Compare it with array element to get maximum result...
+        currSum = Math.max(currSum + nums[i], nums[i]);
+        // Compare current sum and max sum.
+        maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;      // return the contiguous subarray which has the largest sum...
+}
