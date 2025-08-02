e417a4923cb613549d789fcba380b6ed2aaecc6f
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-08-02 21:36:25 +0530

    Time: 26 ms (53.06%), Space: 66.5 MB (44.9%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.js
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.js
@@ -4,14 +4,17 @@
  * @return {number}
  */
 var countCompleteComponents = function(n, edges) {
+    // Step 1: Build adjacency list
     let adj = Array.from({ length: n }, () => []);
     for(let [u,v] of edges){
         adj[u].push(v);
         adj[v].push(u);
     }
 
+    // Step 2: Initialize visited array
     let vis = new Array(n).fill(0);
 
+    // Step 3: DFS to find connected components
     function dfs(node, component) {
         vis[node] = 1;
         component.push(node);
@@ -22,6 +25,7 @@ var countCompleteComponents = function(n, edges) {
         }
     }
 
+    // Step 4: Find connected components
     let components = [];
     for(let i=0; i<n; i++){
         if(!vis[i]){
@@ -31,7 +35,8 @@ var countCompleteComponents = function(n, edges) {
         }
     }
 
-    let comp = 0;
+    // Step 5: Validate complete components
+    let completeComponents = 0;
     for(let component of components){
         const size = component.length;
         let edgesCount = 0;
@@ -39,10 +44,11 @@ var countCompleteComponents = function(n, edges) {
             edgesCount += adj[node].length;
         }
 
+        // Check if the component is complete
         if(edgesCount === size * (size - 1)) {
-            comp++;
+            completeComponents++;
         }
     }
 
-    return comp;
+    return completeComponents;
 };
