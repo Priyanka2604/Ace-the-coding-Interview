885673a81e63b99e1bca8560c2d9930dfddc9774
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-08-03 13:41:51 +0530

    Time: 4 ms (12.08%), Space: 57.1 MB (91.64%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    let rows = image.length;
+    let cols = image[0].length;
+    const currColor = image[sr][sc];
+    if(currColor === color) return image;
+    let directions = [[-1,0], [1,0], [0,-1], [0,1]];
+    let q = [[sr,sc]];
+
+
+
+    while(q.length){
+        let [r, c] = q.shift();
+        image[r][c] = color;
+
+        for(let [dr,dc] of directions){
+            let nr = r+dr, nc = c+dc;
+            if(nr>=0 && nr<rows && nc>=0 && nc<cols && image[nr][nc] === currColor){
+                q.push([nr, nc]);
+            }
+        }
+    }
+    
+    return image;
+};
