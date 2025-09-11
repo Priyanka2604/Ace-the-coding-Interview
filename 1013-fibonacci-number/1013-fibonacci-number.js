743da16e96412247778a1c41dc7e4600bfc6c782
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2025-09-11 10:09:31 +0530

    Time: 49 ms (60.13%), Space: 54 MB (22.23%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.js
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -2,8 +2,12 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n, dp = {}) {
-    if(n<=1) return n;
-    if(dp[n]) return dp[n];
-    return dp[n] = fib(n-1, dp) + fib(n-2, dp);
+var fib = function(n) {
+    let dp = new Array(n+1).fill(-1);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(let i=2; i <= n ;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
 };
