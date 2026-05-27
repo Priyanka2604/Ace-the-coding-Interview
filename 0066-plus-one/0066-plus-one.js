cc8cfafbe0fc6977980f4fae5d6b28582ea8caf6
Priyanka Das <59468515+Priyanka2604@users.noreply.github.com>
2026-05-28 00:44:04 +0530

    Time: 0 ms (100%), Space: 54.3 MB (13.02%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let n = digits.length-1;
+    for(let i=n; i>=0; i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+
+    digits.unshift(1);
+
+    return digits;
+};
